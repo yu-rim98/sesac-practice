996e56781526e82ce2be12210964c76a7acc9696
yu-rim98 <rimmm098@gmail.com>
2025-09-29 17:20:58 +0900

    Props 실습 1, 2의 App 컴포넌트

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -1,13 +1,11 @@
-import Header from "./components/Header";
 import Main from "./components/Main";
-import Footer from "./components/Footer";
+import ProductContainer from "./components/ProductCard/ProductContainer";
 
 function App() {
   return (
     <>
-      <Header />
+      <ProductContainer />
       <Main />
-      <Footer />
     </>
   );
 }
