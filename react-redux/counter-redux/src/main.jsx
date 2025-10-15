fb9ffe999b117121cc85f404e8a3ddd4fce5de76
yu-rim98 <rimmm098@gmail.com>
2025-10-15 15:19:46 +0900

    3. 라우터와 스토어를 Provider 전달

--- react-redux/counter-redux/src/main.jsx
+++ react-redux/counter-redux/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import router from "./router/index.js";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/index.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* 생성한 스토어 전달 */}
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
