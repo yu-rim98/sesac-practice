bf74ac584c9b67bc220565d6d1f190a70ea1ec6e
yu-rim98 <rimmm098@gmail.com>
2025-11-11 10:44:40 +0900

    주석 제거

--- java/practice/src/oop/inheritance/abstract_/practice02/Shape.java
+++ java/practice/src/oop/inheritance/abstract_/practice02/Shape.java
@@ -1,14 +1,5 @@
 package oop.inheritance.abstract_.practice02;
 
-
-//public final void draw() - 템플릿 메서드 (final로 선언)
-//실행 순서: moveTo() → setColor() → drawShape()
-//void moveTo() - 일반 메서드
-//"위치 이동: (x, y)" 출력
-//void setColor() - 일반 메서드
-//"색상 설정: [color]" 출력
-//abstract void drawShape() - 추상 메서드
-//각 도형마다 다르게 구현
 public abstract class Shape {
     protected int x;
     protected int y;
